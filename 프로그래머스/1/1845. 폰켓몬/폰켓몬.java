16fee83f99524ad4f9c70cd05d28067e4e52c5a5
welcomeglory <godbu2021@naver.com>
2024-06-06 17:42:01 +0900

    [level 1] Title: 폰켓몬, Time: 3.21 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class Solution {
+    public int solution(int[] nums) {
+        int max = nums.length/2;
+        
+        HashSet<Integer> hashSet = new HashSet<>();
+        
+        for(int n : nums){
+            hashSet.add(n);//중복제거
+        }
+        if(max >= hashSet.size()){
+            return hashSet.size();
+        }else{
+            return max;
+        }
+    }
+}
