6837e51a49ffd19b5867eefc65b0cab7fbacbe21
welcomeglory <godbu2021@naver.com>
2024-06-07 21:26:45 +0900

    Update 폰켓몬.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,16 +2,23 @@ import java.util.HashSet;
 
 public class Solution {
     public int solution(int[] nums) {
-        int max = nums.length/2;
-        
+        // 최대 선택 가능한 포켓몬의 수를 계산
+        int max = nums.length / 2;
+
+        // 중복을 제거하기 위해 HashSet을 사용
         HashSet<Integer> hashSet = new HashSet<>();
-        
-        for(int n : nums){
-            hashSet.add(n);//중복제거
+
+        // 배열을 순회하며 각 숫자를 HashSet에 추가
+        for (int n : nums) {
+            hashSet.add(n); // 중복된 숫자는 자동으로 제거됨
         }
-        if(max >= hashSet.size()){
+
+        // 최대 선택 가능한 포켓몬의 수와 HashSet의 크기를 비교
+        if (max >= hashSet.size()) {
+            // 선택 가능한 수가 HashSet 크기보다 크거나 같으면, HashSet의 크기를 반환
             return hashSet.size();
-        }else{
+        } else {
+            // 선택 가능한 수가 HashSet 크기보다 작으면, 최대 선택 가능한 수를 반환
             return max;
         }
     }
