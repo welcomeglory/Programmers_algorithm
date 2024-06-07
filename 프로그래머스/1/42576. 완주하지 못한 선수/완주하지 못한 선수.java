d2328429471048548d325cf041645521d0765a0f
welcomeglory <godbu2021@naver.com>
2024-06-07 21:27:08 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,14 +4,34 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> hm = new HashMap<>();
-        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
-        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-        for (String key : hm.keySet()) {
-            if (hm.get(key) != 0){
-                answer = key;
+        // 참가자 명단을 HashMap에 추가
+        for (String player : participant) {
+            if (hm.containsKey(player)) {
+                // 이미 존재하는 키인 경우 값을 1 증가시킴
+                hm.put(player, hm.get(player) + 1);
+            } else {
+                // 존재하지 않는 키인 경우 값을 1로 설정
+                hm.put(player, 1);
             }
         }
+
+        // 완주자 명단을 통해 HashMap 업데이트
+        for (String player : completion) {
+            // 완주자의 수를 1 감소시킴
+            if (hm.containsKey(player)) {
+                hm.put(player, hm.get(player) - 1);
+            }
+        }
+
+        // HashMap을 순회하며 값이 0이 아닌 참가자 찾기
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+
         return answer;
     }
 }
