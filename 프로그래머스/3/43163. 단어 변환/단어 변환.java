c471573dacaf420d1662febbcd05e95846498727
welcomeglory <godbu2021@naver.com>
2024-06-28 09:13:49 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,36 @@
+class Solution {
+    boolean[] visited;
+    int answer = 0;
+    
+    public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+        
+        dfs(begin, target, words, 0);
+        
+        return answer;
+    }
+    
+    public void dfs(String begin, String target, String[] words, int cnt){
+        if(begin.equals(target)){
+            answer = cnt;
+            return;
+        }
+        
+         for(int i=0; i<words.length; i++){
+             if(visited[i])
+                 continue;
+             
+             int check = 0;
+             for(int j=0; j<begin.length(); j++){
+                 if(begin.charAt(j) == words[i].charAt(j))
+                     check++;
+             }
+             
+             if(check == begin.length()-1){
+                 visited[i] = true;
+                 dfs(words[i], target, words, cnt+1);
+                 visited[i] = false;
+             }
+         }
+    }
+}
