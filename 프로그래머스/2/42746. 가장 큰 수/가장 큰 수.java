040ed2fa06b9ae9c201e79505f746468042dc60f
welcomeglory <godbu2021@naver.com>
2024-07-08 15:24:18 +0900

    [level 2] Title: 가장 큰 수, Time: 1.72 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,19 +3,24 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
          // 숫자를 문자열로 변환하여 정렬
-        String[] numsAsStrings = Arrays.stream(numbers)
-                                      .mapToObj(String::valueOf)
-                                      .toArray(String[]::new);
+        //String[] numsAsStrings = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+        
+        String[] answers = new String[numbers.length];
+        
+        // 2. 반복문을 사용하여 각 숫자를 문자열로 변환하여 배열에 저장
+        for (int i = 0; i < numbers.length; i++) {
+            answers[i] = String.valueOf(numbers[i]);
+        }             
         
         // 정렬 기준 설정
-        Arrays.sort(numsAsStrings, (a, b) -> (b + a).compareTo(a + b));
+        Arrays.sort(answers, (a, b) -> (b + a).compareTo(a + b));
         
         // 모든 숫자가 0인 경우 예외 처리
-        if (numsAsStrings[0].equals("0")) {
+        if (answers[0].equals("0")) {
             return "0";
         }
         
         // 정렬된 문자열 배열을 이어붙여서 결과를 만듦
-        return String.join("", numsAsStrings);
+        return String.join("", answers);
     }
 }
