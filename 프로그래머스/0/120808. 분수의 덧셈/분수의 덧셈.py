2bf3d87d604ec3efa9079acf60dbd917055a5cd9
welcomeglory <godbu2021@naver.com>
2024-07-23 18:50:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,19 +1,7 @@
-def gcd(num1, num2):
-    if num1<num2:
-        num1, num2 = num2, num1
-    while num1%num2 != 0:
-        remainder = num1%num2
-        num1 = num2
-        num2 = remainder
-    return num2
+import math
 
-def solution(numer1, denom1, numer2, denom2):
-    denom_gcd = gcd(denom1, denom2)
-    sum_denom = denom_gcd * (denom1//denom_gcd) * (denom2//denom_gcd)
-    sum_numer = numer1*(sum_denom//denom1) + numer2*(sum_denom//denom2)
-    
-    sum_gcd = gcd(sum_numer, sum_denom)
-    sum_numer //= sum_gcd
-    sum_denom //= sum_gcd
-    answer = [sum_numer, sum_denom]
-    return answer
+def solution(denum1, num1, denum2, num2):
+    denum = denum1 * num2 + denum2 * num1
+    num = num1 * num2
+    gcd = math.gcd(denum, num)
+    return [denum/gcd, num/gcd]
