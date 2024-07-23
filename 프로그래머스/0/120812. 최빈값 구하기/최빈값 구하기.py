2920169a09a701fedc96c14ad40f5a52b508dbc1
welcomeglory <godbu2021@naver.com>
2024-07-23 20:43:05 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,17 +1,6 @@
-from collections import Counter
-
 def solution(array):
-    # 1. 배열의 빈도를 계산
-    counter = Counter(array)
-    
-    # 2. 최빈값 찾기
-    max_freq = max(counter.values())
-    
-    # 3. 최빈값이 여러 개인지 확인
-    modes = [key for key, value in counter.items() if value == max_freq]
-    
-    # 4. 최빈값이 하나면 그 값을 반환, 그렇지 않으면 -1 반환
-    if len(modes) == 1:
-        return modes[0]
-    else:
-        return -1
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            array.remove(a)
+        if i == 0: return a
+    return -1
