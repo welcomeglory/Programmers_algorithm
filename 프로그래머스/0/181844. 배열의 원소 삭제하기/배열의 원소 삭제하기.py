49a253e5ffdfbf65329dfd895efb0376bcf5e804
welcomeglory <godbu2021@naver.com>
2024-07-25 14:23:32 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,3 +1,2 @@
 def solution(arr, delete_list):
-    delete_set = set(delete_list)  # delete_list를 집합으로 변환
-    return [x for x in arr if x not in delete_set]  # delete_set에 없는 원소만 포함된 리스트 반환
+    return [i for i in arr if i not in delete_list]
