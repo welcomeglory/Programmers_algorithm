2d171fef724a9490651871c071c63d574cfb5186
welcomeglory <godbu2021@naver.com>
2024-07-25 14:06:55 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,9 +1,5 @@
 def solution(num_list):
-    even_count = 0
-    odd_count = 0
+    answer = [0,0] 
     for num in num_list:
-        if num % 2 == 0:
-            even_count += 1
-        else:
-            odd_count += 1            
-    return [even_count, odd_count]
+        answer[num % 2] += 1                 
+    return answer
