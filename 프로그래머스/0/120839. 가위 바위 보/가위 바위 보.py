879bba9a95078890bc8c704de8b951b14b4a642c
welcomeglory <godbu2021@naver.com>
2024-07-27 16:57:58 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 9.92 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,9 +1,3 @@
 def solution(rsp):
-    # 변환 사전 정의
-    win_map = {
-        '2': '0',  # 가위(2) → 바위(0)
-        '0': '5',  # 바위(0) → 보(5)
-        '5': '2'   # 보(5) → 가위(2)
-    }
-    
-    return ''.join(win_map[char] for char in rsp)
+    win_map = {'2':'0','0':'5','5':'2'}
+    return ''.join([win_map[i] for i in rsp])
