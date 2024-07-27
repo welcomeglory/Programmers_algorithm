f77567da99bf0947f1afa6e5069d90e75f222c05
welcomeglory <godbu2021@naver.com>
2024-07-27 16:39:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,13 +1,3 @@
 def solution(emergency):
-    # 응급도의 순위를 매기기 위해, 응급도와 원래 인덱스를 함께 저장
-    indexed_emergency = list(enumerate(emergency))
-    
-    # 응급도를 기준으로 내림차순 정렬
-    sorted_emergency = sorted(indexed_emergency, key=lambda x: x[1], reverse=True)
-    
-    # 순위 배열을 만들기
-    ranks = [0] * len(emergency)
-    for rank, (index, _) in enumerate(sorted_emergency):
-        ranks[index] = rank + 1
-    
-    return ranks
+    return [sorted(emergency, reverse=True).index(e) + 1 for e in emergency]
+
