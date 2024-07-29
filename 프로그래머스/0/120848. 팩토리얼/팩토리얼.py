1f9e30e48670758b7fb5d60a4f1568d03f58e6d3
welcomeglory <godbu2021@naver.com>
2024-07-29 17:34:59 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,15 +1,7 @@
+from math import factorial
+
 def solution(n):
-    # 초기값 설정
-    i = 1
-    factorial = 1
-    
-    # n의 범위 내에서 최대 팩토리얼 i를 찾기 위한 반복
-    while True:
-        # 현재 팩토리얼 값이 n을 초과하면 종료
-        if factorial > n:
-            return i - 1
-        # 팩토리얼 계산을 계속 진행
-        i += 1
-        factorial *= i
-    
-    return i - 1  # 마지막으로 만족한 i값을 반환
+    k = 10
+    while n < factorial(k):
+        k -= 1
+    return k
