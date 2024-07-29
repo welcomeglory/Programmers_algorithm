3dfd18acb042016c7d0199e4292aae2a125ebcd4
welcomeglory <godbu2021@naver.com>
2024-07-29 17:26:56 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    # 숫자만 추출하여 리스트에 저장
-    numbers = [int(char) for char in my_string if char.isdigit()]
-    
-    # 숫자 리스트를 오름차순으로 정렬
-    numbers.sort()
-    
-    return numbers
+    return sorted([int(c) for c in my_string if c.isdigit()])
