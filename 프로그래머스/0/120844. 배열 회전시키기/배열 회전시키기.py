6f79917ca6cd25b64c35739f6f8afd610c201605
welcomeglory <godbu2021@naver.com>
2024-07-29 17:38:50 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,11 +1,3 @@
 def solution(numbers, direction):
-    n = len(numbers)  # 배열의 길이
+        return [numbers[-1]] + numbers[:-1] if direction == 'right' else numbers[1:] + [numbers[0]]
 
-    if direction == "left":
-        # 왼쪽 회전
-        return numbers[1:] + [numbers[0]]
-    elif direction == "right":
-        # 오른쪽 회전
-        return [numbers[-1]] + numbers[:-1]
-    else:
-        raise ValueError("direction must be either 'left' or 'right'")
