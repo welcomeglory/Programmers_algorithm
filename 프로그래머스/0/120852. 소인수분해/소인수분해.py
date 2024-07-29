58bdd55df20057ca0b1588abc761c2446a2e2323
welcomeglory <godbu2021@naver.com>
2024-07-29 17:25:37 +0900

    [level 0] Title: 소인수분해, Time: 1.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,23 +1,12 @@
 def solution(n):
-    factors = []
-    # 2부터 시작하여 n의 소인수를 찾습니다.
-    # 2는 가장 작은 소수이므로 먼저 처리합니다.
-    while n % 2 == 0:
-        if 2 not in factors:
-            factors.append(2)
-        n //= 2
-    
-    # 3부터 시작하여 홀수 소수들로 나누어가면서 소인수를 찾습니다.
-    factor = 3
-    while factor * factor <= n:
-        while n % factor == 0:
-            if factor not in factors:
-                factors.append(factor)
-            n //= factor
-        factor += 2
-    
-    # n이 1보다 크다면 그것도 소수입니다.
-    if n > 1:
-        factors.append(n)
-    
-    return factors
+    answer = []
+    d = 2
+    while d <= n:
+        if n % d == 0:
+            n /= d
+            if d not in answer:
+                answer.append(d)
+        else:
+            d += 1
+    return answer
+
