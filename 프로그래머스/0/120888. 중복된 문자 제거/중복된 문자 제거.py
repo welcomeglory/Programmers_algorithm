23b14822a2a396809fa79c5f64d8b7875efa2e85
welcomeglory <godbu2021@naver.com>
2024-07-31 11:25:07 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,13 +1,6 @@
 def solution(my_string):
-    # 중복 제거와 순서 유지를 위한 집합
-    seen = set()
-    # 결과를 담을 리스트
-    result = []
-
-    for char in my_string:
-        if char not in seen:
-            seen.add(char)
-            result.append(char)
-    
-    # 리스트를 문자열로 변환하여 반환
-    return ''.join(result)
+    answer = ''
+    for i in my_string:
+        if i not in answer:
+            answer+=i
+    return answer
