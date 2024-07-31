6fc6dfc3e10ec206828203c69c9e0ccefa84c0be
welcomeglory <godbu2021@naver.com>
2024-07-31 11:16:52 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,11 +1,6 @@
 def solution(order):
-    # 숫자를 문자열로 변환
-    order_str = str(order)
-    
-    # 박수를 쳐야 하는 숫자
-    clap_digits = ['3', '6', '9']
-    
-    # 박수를 쳐야 하는 횟수를 계산
-    clap_count = sum(order_str.count(digit) for digit in clap_digits)
-    
-    return clap_count
+    answer = 0
+    for i in str(order):
+        if i in ["3", "6", "9"]:
+            answer += 1
+    return answer
