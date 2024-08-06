69d412c0cdb129fdcf01a47372ab523b0a070b86
welcomeglory <godbu2021@naver.com>
2024-08-06 19:32:16 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 1.35 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -1,20 +1,12 @@
 def solution(arr):
-    rows = len(arr)
-    cols = len(arr[0])
-    
-    if rows > cols:
-        # 열의 수를 행의 수에 맞추기 위해 각 행의 끝에 0을 추가
-        for row in arr:
-            row.extend([0] * (rows - cols))
-        # 추가된 열을 갖는 배열을 리턴
-        return arr
-    elif cols > rows:
-        # 각 행의 길이를 맞추고, 부족한 행을 0으로 채운 행으로 추가
-        # 행 길이를 맞추기
-        new_arr = [row + [0] * (cols - len(row)) for row in arr]
-        # 부족한 행 추가
-        new_arr.extend([[0] * cols for _ in range(cols - rows)])
-        return new_arr
-    
-    # 행과 열의 수가 같은 경우 원본 배열을 그대로 리턴
+    n=len(arr)
+    m=len(arr[0])
+    if n>m:
+        for i in range(n):
+            for j in range(n-m):
+                arr[i].append(0)
+    else:
+        for i in range(m-n):
+            arr.append([0]*m)
+
     return arr
