a3e90bf765b136f9aa17b68f4f14ed9cd6796e4f
welcomeglory <godbu2021@naver.com>
2024-08-06 17:16:39 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,7 @@
+def solution(n):
+    divisors = set()
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            divisors.add(i)
+            divisors.add(n // i)
+    return sorted(divisors)
