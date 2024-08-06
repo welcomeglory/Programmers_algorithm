08c1e8d3a597633b9699fd4e0c72e6d1bfe682bc
welcomeglory <godbu2021@naver.com>
2024-08-06 18:22:23 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,16 +1,2 @@
 def solution(date1, date2):
-    # date1과 date2의 연도, 월, 일 비교
-    if date1[0] < date2[0]:  # 연도 비교
-        return 1
-    elif date1[0] > date2[0]:  # 연도 비교
-        return 0
-    else:
-        if date1[1] < date2[1]:  # 월 비교
-            return 1
-        elif date1[1] > date2[1]:  # 월 비교
-            return 0
-        else:
-            if date1[2] < date2[2]:  # 일 비교
-                return 1
-            else:
-                return 0
+    return int(date1 < date2)
