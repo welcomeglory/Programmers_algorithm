4c33d640a0b2c0b254b8d62acbdd575ce8019272
welcomeglory <godbu2021@naver.com>
2024-08-06 16:53:30 +0900

    [level 0] Title: 문자열 묶기, Time: 8.68 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,15 +1,4 @@
 def solution(strArr):
-    from collections import defaultdict
-    
-    # 길이별 문자열 개수를 저장할 딕셔너리
-    length_count = defaultdict(int)
-    
-    # 문자열의 길이에 따라 개수 세기
-    for s in strArr:
-        length = len(s)
-        length_count[length] += 1
-    
-    # 가장 많이 포함된 그룹의 크기 찾기
-    max_group_size = max(length_count.values())
-    
-    return max_group_size
+    a=[0]*31
+    for x in strArr: a[len(x)]+=1
+    return max(a)
