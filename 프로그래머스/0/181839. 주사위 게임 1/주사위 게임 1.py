dfea4652cafd9f856e592fa0ebfcf5b594e1cbef
welcomeglory <godbu2021@naver.com>
2024-08-06 17:52:48 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,16 +1,6 @@
 def solution(a, b):
-    # 홀수 여부를 체크하는 함수
-    def is_odd(num):
-        return num % 2 != 0
-    
-    # 두 숫자 모두 홀수인 경우
-    if is_odd(a) and is_odd(b):
-        return a**2 + b**2
-    
-    # 둘 중 하나만 홀수인 경우
-    elif is_odd(a) or is_odd(b):
-        return 2 * (a + b)
-    
-    # 둘 다 홀수가 아닌 경우
-    else:
-        return abs(a - b)
+    if a%2 and b%2: 
+        return a*a+b*b
+    elif a%2 or b%2: 
+        return 2*(a+b)
+    return abs(a-b)
