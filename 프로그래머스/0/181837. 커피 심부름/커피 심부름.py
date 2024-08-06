a86dfe7b278cb2ab59c56a692f5cb99e09e4b10b
welcomeglory <godbu2021@naver.com>
2024-08-06 19:21:09 +0900

    [level 0] Title: 커피 심부름, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,26 +1,8 @@
 def solution(order):
-    # 가격 정의
-    price_map = {
-        'americano': 4500,
-        'cafelatte': 5000
-    }
-    
-    # 변환된 가격을 저장할 리스트
-    prices = []
+    answer = 0
+    for want in order:
+        if 'latte' in want:
+            answer += 500
+        answer += 4500
 
-    for item in order:
-        # 'anything'은 'americano'으로 변환
-        if item == 'anything':
-            item = 'americano'
-        
-        # 'ice' 또는 'hot'이 포함된 문자열 처리
-        item = item.replace('ice', '').replace('hot', '')
-
-        # 가격을 가져와 리스트에 추가
-        price = price_map.get(item, 0)
-        prices.append(price)
-    
-    # 총 합계 계산
-    total_price = sum(prices)
-    
-    return total_price
+    return answer
