e26a4a391aa997e7ce2bf1b21c82d85df41bffbc
welcomeglory <godbu2021@naver.com>
2024-08-08 20:55:03 +0900

    [level 0] Title: k의 개수, Time: 38.36 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,10 +1,5 @@
 def solution(i, j, k):
-    k = str(k)  # k를 문자열로 변환
-    count = 0
-    
-    # i부터 j까지 반복
-    for num in range(i, j + 1):
-        num_str = str(num)  # 숫자를 문자열로 변환
-        count += num_str.count(k)  # 문자열에서 k의 등장 횟수를 더함
-
-    return count
+    answer = 0
+    for n in range(i, j + 1):
+        answer += str(n).count(str(k))
+    return answer
