5fcc12fbdcc7778b1a77cd631078d3a96d41f153
welcomeglory <godbu2021@naver.com>
2024-08-09 19:30:27 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,10 +1,5 @@
 def solution(spell, dic):
-    # spell을 사전순으로 정렬하여 문자열로 변환
-    spell_sorted = ''.join(sorted(spell))
-    
-    for word in dic:
-        # dic의 각 단어를 정렬하여 비교
-        if ''.join(sorted(word)) == spell_sorted:
+    for d in dic:
+        if sorted(d) == sorted(spell):
             return 1
-    
     return 2
