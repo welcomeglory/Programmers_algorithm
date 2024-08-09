81555676e9113cf2d6a8c2a64bfb1b2504f272ba
welcomeglory <godbu2021@naver.com>
2024-08-09 20:36:16 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,12 +1,4 @@
 def solution(id_pw, db):
-    user_id, user_pw = id_pw  # 입력된 아이디와 비밀번호
-    
-    for entry in db:
-        db_id, db_pw = entry  # 데이터베이스의 각 회원 정보
-        if db_id == user_id:
-            if db_pw == user_pw:
-                return "login"  # 아이디와 비밀번호가 모두 일치하는 경우
-            else:
-                return "wrong pw"  # 아이디는 일치하지만 비밀번호가 일치하지 않는 경우
-
-    return "fail"  # 일치하는 아이디가 없는 경우
+    if db_pw := dict(db).get(id_pw[0]):
+        return "login" if db_pw == id_pw[1] else "wrong pw"
+    return "fail"
