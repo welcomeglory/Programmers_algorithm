98890d8c556b3c1f44b2ba995634eca704501ace
welcomeglory <godbu2021@naver.com>
2024-08-09 19:29:09 +0900

    [level 0] Title: 안전지대, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,25 +1,9 @@
-def check(x,y,l,board):
-    x,y=x-1,y-1
-    for i in range(x,x+3):
-        for j in range(y,y+3):
-            if 0<=i<l and 0<=j<l:
-                board[i][j]=1  
-
 def solution(board):
-    answer = 0
-    coor = []
-    l = len(board)
-    for i in range(l):
-        for j in range(l):
-            if board[i][j] == 1:
-                coor.append([i,j])
-    for c in coor:
-        x,y=c
-        check(x,y,l,board)
-
-    print(board)
-
-    for b in board:
-        answer += b.count(0)
-
-    return answer
+    n = len(board)
+    danger = set()
+    for i, row in enumerate(board):
+        for j, x in enumerate(row):
+            if not x:
+                continue
+            danger.update((i+di, j+dj) for di in [-1,0,1] for dj in [-1, 0, 1])
+    return n*n - sum(0 <= i < n and 0 <= j < n for i, j in danger)
