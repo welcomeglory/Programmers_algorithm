5f6c348d1f3fe8890e41841a488b9c22b8255bd3
welcomeglory <godbu2021@naver.com>
2024-08-13 13:40:45 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = 0
+    for i in range(0, n):
+        while answer % 3 == 0 or "3" in str(answer):
+            answer += 1
+        answer += 1
+
+    return answer - 1
