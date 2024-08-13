ca7bc59bf73247af807deb11c101249713d38681
welcomeglory <godbu2021@naver.com>
2024-08-13 18:10:38 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,3 +1,2 @@
 def solution(num, total):
-    average = total // num
-    return [i for i in range(average - (num-1)//2, average + (num + 2)//2)]
+    return [(total - (num * (num - 1) // 2)) // num + i for i in range(num)]
