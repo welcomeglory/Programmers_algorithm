a4d293dad61fe50bf2c2cc06a7d7489e0159c23f
welcomeglory <godbu2021@naver.com>
2024-08-13 14:06:24 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,3 +1,3 @@
 def solution(numlist, n):
-        return list(map(lambda x: x[1], sorted(map(lambda x: [abs(n - x), x], numlist), key=lambda x: [x[0], -x[1]])))
+    return sorted(numlist,key = lambda x: [abs(x-n),-x])
 
