dcb1c93b11ed5b72959b5dd1a5b0a8b8643708a1
welcomeglory <godbu2021@naver.com>
2024-08-13 13:56:25 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,26 +1,9 @@
-def get_factors(n):
-    factors = []
-    div = 2
-    while n > 1:
-        if n % div == 0:
-            factors.append(div)
-            n //= div
-        else:
-            div += 1
-    return factors
+from math import gcd
 
 def solution(a, b):
-    answer = 1
-    for i in range(2, 1001):
-        if a % i == 0 and b % i == 0:
-            a //= i
+    b = b / gcd(a, b)
+    for i in [2, 5]:
+        while not b % i:
             b //= i
 
-
-    factors = get_factors(b)
-    print(factors)
-
-    if not all(x in [2,5] for x in factors):
-        answer = 2
-
-    return answer
+    return 1 if b == 1 else 2
