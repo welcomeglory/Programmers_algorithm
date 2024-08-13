d9bcdb6eab37960b58c41e2fc3784b113cbc1613
welcomeglory <godbu2021@naver.com>
2024-08-13 18:12:50 +0900

    [level 0] Title: 옹알이 (1), Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,23 +1,9 @@
-from itertools import permutations
-
-
 def solution(babbling):
-    count = 0
-    # 1, 2, 3, 4가지 경우에 대해 permutations 구하기
-    basic_babblings = ["aya", "ye", "woo", "ma"]
-    all_babblings = []
-    for i in range(1, 5):
-        perms = list(permutations(basic_babblings, i))
-
-        for perm in perms:
-            s = ''
-            for element in perm:
-                s += element
-            all_babblings.append(s)
-
-    for babblings in all_babblings:
-        for babble in babbling:
-            if babblings == babble:
-                count += 1
-
-    return count
+    c = 0
+    for b in babbling:
+        for w in [ "aya", "ye", "woo", "ma" ]:
+            if w * 2 not in b:
+                b = b.replace(w, ' ')
+        if len(b.strip()) == 0:
+            c += 1
+    return c
