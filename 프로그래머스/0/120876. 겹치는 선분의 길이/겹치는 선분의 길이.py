bd5d5656bbdede465233e2a731d4fd17a70f67b1
welcomeglory <godbu2021@naver.com>
2024-08-13 18:11:24 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,16 +1,3 @@
 def solution(lines):
-    answer = 0 
-    lines.sort(key=lambda x: x[0])
-    cs, ce = lines[0]
-
-    for line in lines[1:]:
-        s, e = line
-        if ce <= s:
-            cs, ce = s, e
-            continue
-
-        maxs, mine, maxe = max(cs, s), min(ce, e), max(ce, e)
-        answer += abs(mine - maxs)
-        cs, ce = mine, maxe
-
-    return answer
+    sets = [set(range(min(l), max(l))) for l in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
