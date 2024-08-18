55092065674f801518c7f06b84cb0b83409639ce
welcomeglory <godbu2021@naver.com>
2024-08-18 16:40:25 +0900

    [level 1] Title: 약수의 합, Time: 0.27 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,12 +1,2 @@
 def solution(n):
-    if n == 0:
-        return 0
-    
-    sum_of_divisors = 0
-    for i in range(1, int(n**0.5) + 1):
-        if n % i == 0:
-            sum_of_divisors += i
-            if i != n // i:  # Avoid adding the square root twice when n is a perfect square
-                sum_of_divisors += n // i
-    
-    return sum_of_divisors
+    return sum([i for i in range(1,n+1) if n%i==0])
