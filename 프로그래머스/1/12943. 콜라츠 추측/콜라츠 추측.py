a5ef90cd1a1eb9ca90f085934e965340e8a60cae
welcomeglory <godbu2021@naver.com>
2024-08-18 19:02:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,2 +1,8 @@
 def solution(num):
-    return (lambda c: c if c < 500 and num == 1 else -1)(len([num for _ in range(500) if num != 1 and (num := num // 2 if num % 2 == 0 else num * 3 + 1)]))
+    count = 0
+    while num != 1 and count < 500:
+        num = num // 2 if num % 2 == 0 else num * 3 + 1
+        count += 1
+    return count if num == 1 else -1
+
+
