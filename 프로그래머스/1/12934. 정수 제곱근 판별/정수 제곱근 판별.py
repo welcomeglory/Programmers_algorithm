a52b65cd23d2530b92657a211c1c9f65647a433c
welcomeglory <godbu2021@naver.com>
2024-08-18 16:50:15 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,4 +1,4 @@
 import math
 def solution(n):
     answer = math.sqrt(n)
-    return (answer+1)*(answer+1) if answer == int(answer) else -1
+    return (answer+1)**2 if answer == int(answer) else -1
