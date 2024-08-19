d9accd66ef1d8a424ea058088ffe18ab440304a5
welcomeglory <godbu2021@naver.com>
2024-08-19 13:22:11 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 2.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,14 +1,8 @@
-def count_divisors(n):
-    count = 0
-    for i in range(1, n + 1):
-        if n % i == 0:
-            count += 1
-    return count
 def solution(left, right):
-    result = 0
-    for num in range(left, right + 1):
-        if count_divisors(num) % 2 == 0:
-            result += num
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
         else:
-            result -= num
-    return result
+            answer += i
+    return answer
