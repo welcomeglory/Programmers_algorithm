23ee1e6e51818c0294b323a3fb0925b457f45ccb
welcomeglory <godbu2021@naver.com>
2024-08-19 11:42:22 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,4 +1,5 @@
 def solution(s):
     s_list = list(s)
     s_len = len(s_list)
-    return "".join(s_list[s_len//2] if s_len % 2 == 1 else s_list[s_len//2-1:s_len//2+1])
+    # return "".join(s_list[s_len//2] if s_len % 2 == 1 else s_list[s_len//2-1:s_len//2+1])
+    return s[(len(s)-1)//2 : len(s)//2 + 1]
