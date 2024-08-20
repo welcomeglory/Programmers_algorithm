7f48a4d9df3ef1526e57d1a99b0ab0470bf6a8ea
welcomeglory <godbu2021@naver.com>
2024-08-20 21:29:52 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,9 +1,2 @@
 def solution(price, money, count):
-    # 총 필요한 금액을 계산
-    total_cost = price * (count * (count + 1)) // 2
-    
-    # 부족한 금액을 계산
-    shortfall = total_cost - money
-    
-    # 부족한 금액이 0보다 작거나 같으면 0을 반환하고, 그렇지 않으면 부족한 금액을 반환
-    return max(shortfall, 0)
+    return max(0,price*(count+1)*count//2-money)
