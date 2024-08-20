e2825f6c6c9f1f175092fe4d12fe03bc872c4e25
welcomeglory <godbu2021@naver.com>
2024-08-20 21:37:53 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,10 +1,3 @@
 def solution(s):
-    # 문자열 s를 공백을 기준으로 분리하여 리스트로 변환
-    numbers = list(map(int, s.split()))
-    
-    # 최소값과 최대값을 찾음
-    min_val = min(numbers)
-    max_val = max(numbers)
-    
-    # 결과를 형식에 맞게 문자열로 반환
-    return f"{min_val} {max_val}"
+    s = list(map(int,s.split()))
+    return str(min(s)) + " " + str(max(s))
