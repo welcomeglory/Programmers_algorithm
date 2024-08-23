26f66a8cd7f6d271c1eab3d2ee371bd43b482491
welcomeglory <godbu2021@naver.com>
2024-08-23 17:16:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,15 +1,4 @@
 def solution(s):
-    words = s.split(" ")
-    capitalized_words = [word.capitalize() for word in words]
-    return ' '.join(capitalized_words)
-#     s = s.lower()
-#     s_listed = list(s)
-#     if s_listed[0].isalpha():
-#         s_listed[0] = s_listed[0].upper()
+    return ' '.join([word.capitalize() for word in s.split(" ")])
 
-#     for i in range(1,len(s_listed)):
-#         if s_listed[i] == " ":
-#             s_listed[i+1] = s_listed[i+1].upper()
-            
-#     return "".join(s_listed)
     
