5f8af78a5fdde007dba48d94aa6ed8c63ad986b6
welcomeglory <godbu2021@naver.com>
2024-08-31 03:42:16 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,7 +1,10 @@
-def solution (number) :
-    from itertools import combinations
-    cnt = 0
-    for i in combinations(number,3) :
-        if sum(i) == 0 :
-            cnt += 1
-    return cnt
+def solution(number):
+    answer = 0
+    l = len(number)
+    for i in range(l-2):
+        for j in range(i+1, l-1):
+            for k in range(j+1, l):
+                # print(number[i],number[j],number[k])
+                if number[i]+number[j]+number[k] == 0:
+                    answer += 1           
+    return answer
