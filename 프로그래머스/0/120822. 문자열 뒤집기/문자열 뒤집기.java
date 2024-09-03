feacbeffdea3c46e05441eb58ef693c9583c1d2f
welcomeglory <godbu2021@naver.com>
2024-09-03 23:12:21 +0900

    [level 0] Title: 문자열 뒤집기, Time: 34.72 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,5 +1,9 @@
 class Solution {
-    public String solution(String my_string) {      
-        return new StringBuffer(my_string).reverse().toString();    
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=my_string.length()-1; i>=0; i--){
+            answer+=my_string.charAt(i);
+        }
+        return answer;
     }
 }
